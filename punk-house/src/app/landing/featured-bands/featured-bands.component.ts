7c6c73e904de5b4bac2aeb513d2b44ac3dbdcd57
Alex Lloyd <lloyd.da89@gmail.com>
2020-02-02 18:36:08 -0500

    more updates

--- punk-house/src/app/landing/featured-bands/featured-bands.component.ts
+++ punk-house/src/app/landing/featured-bands/featured-bands.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 import { FeaturedBandsService } from "../featured-bands.service";
 import { FeaturedBand } from "src/app/models/featured-bands";
 
 @Component({
   selector: "app-featured-bands",
   templateUrl: "./featured-bands.component.html",
-  styleUrls: ["./featured-bands.component.scss"]
+  styleUrls: ["./featured-bands.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeaturedBandsComponent implements OnInit {
   featuredBands: FeaturedBand[] = [];
